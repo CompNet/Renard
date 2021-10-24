48d195348d360bcb6628f98e0cdbaf5f35cc102a
Aethor <oow.autre@gmail.com>
2021-10-24 23:24:51 +0200

    added basic preprocessing with regex substitution

---
+++ renard/preprocessing.py
@@ -0,0 +1,25 @@
+from typing import List, Tuple, Set, Dict, Any
+import re
+from pipeline import PipelineStep
+
+
+class CustomSubstitutionPreprocessor(PipelineStep):
+    """A preprocessor alowing regex-based substition
+
+    :ivar substition_rules: A list of rules, each rule being of the
+        form (match, substitution).
+    """
+
+    def __init__(self, substition_rules: List[Tuple[str, str]]) -> None:
+        self.substition_rules = substition_rules
+
+    def __call__(self, text: str, **kwargs) -> Dict[str, Any]:
+        for rule in self.substition_rules:
+            text = re.sub(rule[0], rule[1], text)
+        return {"text": text}
+
+    def needs(self) -> Set[str]:
+        return set()
+
+    def produces(self) -> Set[str]:
+        return set()
