c581c9d62a00c91b57708b63ba74093a03dbf96d
Aethor <oow.autre@gmail.com>
2023-01-23 11:46:03 +0100

    fix test_nltk_tokens_and_sentences_are_aligned flakiness

--- tests/test_tokenization.py
+++ tests/test_tokenization.py
@@ -8,11 +8,21 @@ from renard.pipeline.tokenization import NLTKTokenizer, BertTokenizer
 from renard.pipeline.progress import get_progress_reporter
 
 
-@given(input_text=st.text())
-def test_nltk_tokens_and_sentences_are_aligned(input_text: str):
+@fixture
+def nltk_tokenizer() -> NLTKTokenizer:
     tokenizer = NLTKTokenizer()
     tokenizer._pipeline_init("eng", get_progress_reporter(None))
-    out_dict = tokenizer(input_text)
+    return tokenizer
+
+
+# we suppress the `function_scoped_fixture` health check since we want
+# to execute the `bert_tokenizer` fixture only once.
+@given(input_text=st.text())
+@settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+def test_nltk_tokens_and_sentences_are_aligned(
+    input_text: str, nltk_tokenizer: NLTKTokenizer
+):
+    out_dict = nltk_tokenizer(input_text)
     assert out_dict["tokens"] == list(flatten(out_dict["sentences"]))
     assert out_dict["tokens"] == list(flatten(out_dict["chapter_tokens"]))
 
