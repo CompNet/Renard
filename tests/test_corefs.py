1074cf38ab6ba9729503c651037be02a99a545df
Aethor <oow.autre@gmail.com>
2023-11-13 16:05:39 +0100

    BertCoreferenceResolver: respect standard progress reporting, now correctly take device into account

--- tests/test_corefs.py
+++ tests/test_corefs.py
@@ -1,14 +1,15 @@
 import os
 import pytest
-from renard.pipeline.corefs import BertCoreferenceResolver, Mention
+from renard.pipeline import Pipeline
+from renard.pipeline.corefs import BertCoreferenceResolver
 
 
 @pytest.mark.skipif(os.getenv("RENARD_TEST_ALL") != "1", reason="performance")
 def test_bert_coreference_resolver_runs():
-    coref_step = BertCoreferenceResolver()
-    coref_step._pipeline_init_("eng", None)  # type: ignore
+    pipeline = Pipeline([BertCoreferenceResolver()], progress_report=None)
     tokens = "Princess Liana felt sad , because Zarth Arn was gone . The princess went to sleep .".split(
         " "
     )
-    corefs = coref_step(tokens)["corefs"]
+    corefs = pipeline(tokens=tokens).corefs
+    assert not corefs is None
     assert len(corefs) == 2
