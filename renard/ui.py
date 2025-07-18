ead4a8956e515e41b63ed2364a4ba6a7a28ee1b3
Aethor <oow.autre@gmail.com>
2025-07-18 23:51:19 +0800

    add param description in renard.ui

--- renard/ui.py
+++ renard/ui.py
@@ -21,6 +21,7 @@ import gradio as gr
 @dataclass
 class PipelineStepBuilder:
     cls: Type[PipelineStep]
+    description: str
     # { name => (type, default value) }
     kwargs: dict[str, tuple[Union[Type, str], Any]]
 
@@ -31,26 +32,34 @@ class PipelineStepBuilder:
         return self.cls(**step_kwargs)
 
 
-tokenization_step_builders = [PipelineStepBuilder(NLTKTokenizer, {})]
+tokenization_step_builders = [PipelineStepBuilder(NLTKTokenizer, "", {})]
 ner_step_builders = [
-    PipelineStepBuilder(NLTKNamedEntityRecognizer, {}),
-    PipelineStepBuilder(BertNamedEntityRecognizer, {}),
+    PipelineStepBuilder(NLTKNamedEntityRecognizer, "Fast statistical NER model", {}),
+    PipelineStepBuilder(
+        BertNamedEntityRecognizer, "BERT based deep learning NER model", {}
+    ),
 ]
-coref_step_builders = [PipelineStepBuilder(BertCoreferenceResolver, {})]
+coref_step_builders = [
+    PipelineStepBuilder(
+        BertCoreferenceResolver, "BERT based deep learning coreference model", {}
+    )
+]
 character_unification_step_builders = [
     PipelineStepBuilder(
         GraphRulesCharacterUnifier,
+        "Rules based character unification algorithm",
         {
             "min_appearances": ("uint", 0),
             "link_corefs_mentions": (bool, False),
             "ignore_leading_determiner": (bool, False),
         },
     ),
-    PipelineStepBuilder(NaiveCharacterUnifier, {}),
+    PipelineStepBuilder(NaiveCharacterUnifier, "Baseline naive character unifier", {}),
 ]
 graph_extraction_step_builder = [
     PipelineStepBuilder(
         CoOccurrencesGraphExtractor,
+        "",
         {"co_occurrences_dist": ("uint", 25)},
     )
 ]
@@ -180,10 +189,10 @@ def render_kwargs_(step_builder: PipelineStepBuilder, kwargs: gr.State):
         if typ == str:
             tbox = gr.Textbox(label=name, value=default, interactive=True, key=key)
             tbox.change(set_kwargs, [kwargs, gr.State(name), tbox], [kwargs])
-        if typ == int:
+        elif typ == int:
             nb = gr.Number(label=name, value=float(default), interactive=True, key=key)
             nb.change(set_kwargs, [kwargs, gr.State(name), nb], [kwargs])
-        if typ == "uint":
+        elif typ == "uint":
             uint = gr.Number(
                 label=name, value=float(default), minimum=0, interactive=True, key=key
             )
@@ -199,67 +208,75 @@ with gr.Blocks(title="Renard") as demo:
     with gr.Row():
         # Inputs
         with gr.Column():
-            with gr.Accordion("Tokenization", open=False):
-                tok_kwargs = gr.State({})
-                tok_ddown = gr.Dropdown(
-                    [s.name() for s in tokenization_step_builders],
-                    value=tokenization_step_builders[0].name(),
-                    label="Tokenization step",
-                )
-                tok_ddown.change(lambda: {}, [], [tok_kwargs])
-
-                @gr.render(inputs=tok_ddown)
-                def render_tok_kwargs(tok_step: str):
-                    step_builder = select_step_builder(
-                        tokenization_step_builders, tok_step
+            with gr.Group("Tokenization"):
+                gr.Markdown("## Step 1: Tokenization")
+                with gr.Accordion("Click to expand", open=False):
+                    tok_kwargs = gr.State({})
+                    tok_ddown = gr.Dropdown(
+                        [s.name() for s in tokenization_step_builders],
+                        value=tokenization_step_builders[0].name(),
+                        label="Tokenization step",
                     )
-                    render_kwargs_(step_builder, tok_kwargs)
+                    tok_ddown.change(lambda: {}, [], [tok_kwargs])
 
-            with gr.Accordion("NER", open=False):
-                ner_kwargs = gr.State({})
-                ner_ddown = gr.Dropdown(
-                    [s.name() for s in ner_step_builders],
-                    value=ner_step_builders[0].name,
-                    label="NER step",
-                )
-                ner_ddown.change(lambda: {}, [], [ner_kwargs])
+                    @gr.render(inputs=tok_ddown)
+                    def render_tok_kwargs(tok_step: str):
+                        step_builder = select_step_builder(
+                            tokenization_step_builders, tok_step
+                        )
+                        render_kwargs_(step_builder, tok_kwargs)
 
-                @gr.render(inputs=ner_ddown)
-                def render_ner_kwargs(ner_step: str):
-                    ner_builder = select_step_builder(ner_step_builders, ner_step)
-                    render_kwargs_(ner_builder, ner_kwargs)
-
-            with gr.Accordion("Character Unification", open=False):
-                cu_kwargs = gr.State({})
-                cu_ddown = gr.Dropdown(
-                    [s.name() for s in character_unification_step_builders],
-                    value=character_unification_step_builders[0].name(),
-                    label="Character unification step",
-                )
-                cu_ddown.change(lambda: {}, [], [cu_kwargs])
-
-                @gr.render(inputs=cu_ddown)
-                def render_cu_kwargs(cu_step: str):
-                    step_builder = select_step_builder(
-                        character_unification_step_builders, cu_step
+            with gr.Group("NER"):
+                gr.Markdown("## Step 2: Named Entity Recognition")
+                with gr.Accordion("Click to expand", open=False):
+                    ner_kwargs = gr.State({})
+                    ner_ddown = gr.Dropdown(
+                        [s.name() for s in ner_step_builders],
+                        value=ner_step_builders[0].name,
+                        label="NER step",
                     )
-                    render_kwargs_(step_builder, cu_kwargs)
+                    ner_ddown.change(lambda: {}, [], [ner_kwargs])
 
-            with gr.Accordion("Graph Extraction", open=False):
-                ge_kwargs = gr.State({})
-                ge_ddown = gr.Dropdown(
-                    [s.name() for s in graph_extraction_step_builder],
-                    value=graph_extraction_step_builder[0].name(),
-                    label="Graph extraction step",
-                )
-                ge_ddown.change(lambda: {}, [], [ge_kwargs])
+                    @gr.render(inputs=ner_ddown)
+                    def render_ner_kwargs(ner_step: str):
+                        ner_builder = select_step_builder(ner_step_builders, ner_step)
+                        render_kwargs_(ner_builder, ner_kwargs)
 
-                @gr.render(inputs=ge_ddown)
-                def render_ge_kwargs(ge_step: str):
-                    step_builder = select_step_builder(
-                        graph_extraction_step_builder, ge_step
+            with gr.Group("Character Unification"):
+                gr.Markdown("## Step 3: Character Unification")
+                with gr.Accordion("Click to expand", open=False):
+                    cu_kwargs = gr.State({})
+                    cu_ddown = gr.Dropdown(
+                        [s.name() for s in character_unification_step_builders],
+                        value=character_unification_step_builders[0].name(),
+                        label="Character unification step",
                     )
-                    render_kwargs_(step_builder, ge_kwargs)
+                    cu_ddown.change(lambda: {}, [], [cu_kwargs])
+
+                    @gr.render(inputs=cu_ddown)
+                    def render_cu_kwargs(cu_step: str):
+                        step_builder = select_step_builder(
+                            character_unification_step_builders, cu_step
+                        )
+                        render_kwargs_(step_builder, cu_kwargs)
+
+            with gr.Group("Graph Extraction"):
+                gr.Markdown("## Step 4: Graph Extraction")
+                with gr.Accordion("Click to expand", open=False):
+                    ge_kwargs = gr.State({})
+                    ge_ddown = gr.Dropdown(
+                        [s.name() for s in graph_extraction_step_builder],
+                        value=graph_extraction_step_builder[0].name(),
+                        label="Graph extraction step",
+                    )
+                    ge_ddown.change(lambda: {}, [], [ge_kwargs])
+
+                    @gr.render(inputs=ge_ddown)
+                    def render_ge_kwargs(ge_step: str):
+                        step_builder = select_step_builder(
+                            graph_extraction_step_builder, ge_step
+                        )
+                        render_kwargs_(step_builder, ge_kwargs)
 
             # TODO: pipeline level parameter like 'lang'
             text = gr.TextArea()
